a5b9a15288db43b7fa13bb33b8e8dce7a5d97a58
wuqiangroy <wuqiangroy@live.com>
2016-11-10 23:41:27 +0800

    Delete person.py

--- person.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class person(object):
-    def __init__(self, name):
-        self.name = name
-
-    def talk(self, words):
-        print "{0}: \"{1}\"" .format(self.name, words)
