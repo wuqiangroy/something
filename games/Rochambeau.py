e25878dec47db234c0f79ef866720901fb37b6fd
wuqiangroy <wuqiangroy@live.com>
2016-11-14 21:14:24 +0800

    Add files via upload

---
+++ games/Rochambeau.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 石头剪刀布游戏
+'''
+Created on 2016/11/14
+
+Created by WuQiang
+'''
+
+from random import choice
+
+li = ['石头', '剪刀', '布']
+rules = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头'], ]
+while True:
+    n = raw_input('请输入你出的： \n')
+    N = choice(li)
+    if n == N:
+        print "你出 %s， 电脑出 %s， 平局!" % (n, N)
+    elif [n, N] in rules:
+        print "你出 %s， 电脑出 %s， 恭喜你获胜!" % (n, N)
+    else:
+        print "你出 %s， 电脑出 %s， 很遗憾，你输了!" % (n, N)
+
+
+
