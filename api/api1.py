2a4ce26f54f8799af4fb8411121803cbcf4cfa26
wuqiangroy <wuqiangroy@live.com>
2017-03-06 22:49:39 +0800

    fufill api1

--- api/api1.py
+++ api/api1.py
@@ -9,12 +9,12 @@ from urlparse import parse_qs
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    params = parse_qs(environ['QUERY_STRING'])
-    name = params.get('name', [''])[0]
-    website = params.get('website', [''])[0]
-    user = params.get('user', [''])[0]
-    password = params.get('pass', [''])[0]
-    dic = [{'name': name, 'website': website, 'user': user,
-            'password': password}]
-    data = {'data': dic}
-    return json.dumps(data)
+    url = "http://%s?%s" % (environ['HTTP_HOST'], environ['QUERY_STRING'])
+    if len(environ['QUERY_STRING']) > 0:
+        query = urlparse.urlparse(url).query
+        dic = dict([(k, v[0]) for k, v in urlparse.parse_qs(query).items])
+        data = {'data': dic}
+        return json.dumps(data)
+    else:
+        return json.dumps({'data': url})
+
