7231e6376ed2b16bb61f96b3713d7707f50ed1cf
wuqiangroy <wuqiangroy@live.com>
2017-04-03 01:00:39 +0800

    update

--- cookbook/string_and_text/2.7.py
+++ cookbook/string_and_text/2.7.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+"""shortest match"""
+
+import re
+
+text1 = "I want to say:'Hello, World!'"
+text2 = "I love this 'lovely' world, and 'ugly' people."
+
+patch = re.compile(r"\'(.*)\'")
+print(patch.findall(text1))
+# 中间的字符被匹配出来 ["lovely' world, and 'ugly"]
+print(patch.findall(text2))
+
+patch2 = re.compile(r"\'(.*?)\'")
+print(patch2.findall(text1))
+# 最短匹配 ['lovely', 'ugly']
+print(patch2.findall(text2))
--- cookbook/string_and_text/2.8.py
+++ cookbook/string_and_text/2.8.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+"""unicode"""
+
+import unicodedata
+
+s1 = "spicy jalape\u00f1o"
+s2 = "spicy jalapen\u0303o"
+print(s1, s2)
+print(s1 == s2)
+
+t1 = unicodedata.normalize("NFC", s1)
+t2 = unicodedata.normalize("NFC", s2)
+print(t1, t2)
+print(t1 == t2)
+
+print(ascii(t2))
