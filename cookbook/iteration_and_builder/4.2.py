ec5460eef932dc62eab59f8aa7a740957d9aa882
wuqiangroy <wuqiangroy@live.com>
2017-05-03 23:53:33 +0800

    add new file

---
+++ cookbook/iteration_and_builder/4.2.py
@@ -0,0 +1,30 @@
+#!usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+# 委托迭代
+
+
+class Node(object):
+
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return "Node({!r})".format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+if __name__ == "__main__":
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+
+    for i in root:
+        print(i)
