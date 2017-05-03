82ce29c7f2a8e563fae47280f63daa56bf933874
wuqiangroy <wuqiangroy@live.com>
2017-05-04 00:17:55 +0800

    fix bug

--- cookbook/iteration_and_builder/4.3.py
+++ cookbook/iteration_and_builder/4.3.py
@@ -10,6 +10,21 @@ def frange(start, end, step=1):
         yield x
         x += step
 
+
+# yield底层机智
+def countdown(n):
+    print("Starting count from", n)
+    while n > 0:
+        yield n
+        n -= 1
+    print("Finish")
+
 if __name__ == "__main__":
-    for n in frange(0, 4, 0.5):
-        print(n)
+    # for n in frange(0, 4, 0.5):
+    #     print(n)
+    c = countdown(3)
+    print(next(c))
+    print(next(c))
+    print(next(c))
+
+
