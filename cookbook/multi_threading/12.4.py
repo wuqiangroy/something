7682cbffc4309daa7d8a54b1fb8fdb49abd5d1b7
wuqiangroy <wuqiangroy@live.com>
2017-05-21 23:24:30 +0800

    little change

--- cookbook/multi_threading/12.4.py
+++ cookbook/multi_threading/12.4.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+
+"""對臨界區加鎖"""
+
+import threading
+
+
+class SharedCounter(object):
+
+    def __init__(self, initial_value=0):
+        self._value = initial_value
+        self._value_lock = threading.Lock()
+
+    def incr(self, delta=1):
+
+        self._value_lock.acquire()
+        self._value += delta
+        self._value_lock.release()
+
+        # with self._value_lock:
+        #     self._value += delta
+
+    def decr(self, delta=1):
+        self._value_lock.acquire()
+        self._value -= delta
+        self._value_lock.release()
+
+        # with self._value_lock:
+        #     self._value -= delta
