ca4fab87e051d59d64c7d703592561f99071b143
wuqiangroy <wuqiangroy@live.com>
2017-07-08 12:00:10 +0800

    database

--- django_learning/django_blog/blog/models.py
+++ django_learning/django_blog/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    email = models.EmailField()
+    password = models.CharField(max_length=32)
+
+    # shell中User显示<User: username>而不是<User: User objects>
+    def __str__(self):
+        return self.username
