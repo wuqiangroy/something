8496aa49e60b619de35175076c0301b003e8137d
wuqiangroy <wuqiangroy@live.com>
2017-07-09 23:02:54 +0800

    test db filter\all\update\delete\sort

--- django_learning/django_blog/blog/models.py
+++ django_learning/django_blog/blog/models.py
@@ -9,3 +9,31 @@ class User(models.Model):
     # shell中User显示<User: username>而不是<User: User objects>
     def __str__(self):
         return self.username
+
+
+class Author(models.Model):
+    """和Article是一对多关系"""
+    user = models.CharField(max_length=32)
+    email = models.EmailField()
+    add = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.user
+
+
+class Article(models.Model):
+    """和tag是多对多关系"""
+    title = models.CharField(max_length=64)
+    author = models.ForeignKey(Author)
+    content = models.TextField()
+    tag = models.ManyToManyField("Tag")
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.tag_name
