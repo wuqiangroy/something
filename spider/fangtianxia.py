6dab51390725388c57d651420e41dd8a2e336dfa
wuqiangroy <wuqiangroy@live.com>
2017-07-18 23:58:15 +0800

    ^_^

--- spider/fangtianxia.py
+++ spider/fangtianxia.py
@@ -1,7 +1,9 @@
 #!usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+import json
 import requests
+from math import 
 from bs4 import BeautifulSoup
 
 
@@ -12,25 +14,42 @@ def get_data(city):
         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
                       "(KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
     }
-    url = "http://newhouse.cd.fang.com/house/s/piduqu/b1pa/?ctm=1.cd.xf_search.list_type.4"
-    url2 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd/?ctm=2.cd.xf_search.list_type.4"
-    url3 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd-b92/?ctm=1.cd.xf_search.page.2"
-    url4 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd-b93/?ctm=1.cd.xf_search.page.4"
-    url5 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd-b94/?ctm=1.cd.xf_search.page.6"
-    house = []
-    res = requests.get(url3, headers=headers)
+    # # 郫都区
+    # url2 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd/?ctm=2.cd.xf_search.list_type.4"
+    # url3 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd-b92/?ctm=1.cd.xf_search.page.2"
+    # url4 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd-b93/?ctm=1.cd.xf_search.page.4"
+    # # url5 = "http://newhouse.cd.fang.com/house/s/piduqu/b1pd-b94/?ctm=1.cd.xf_search.page.6"
+    # # 温江
+    # urlw1 = "http://newhouse.cd.fang.com/house/s/wenjiang/b1pd/?ctm=1.cd.xf_search.list_type.4"
+    # urlw2 = "http://newhouse.cd.fang.com/house/s/wenjiang/b1pd-b92/"
+    url = "http://newhouse.cd.fang.com/house/s/{}/b1pd-b91/".format(city)
+    house = {}
+    res = requests.get(url, headers=headers)
     res = res.text.encode("ISO-8859-1")
     soup = BeautifulSoup(res, "html5lib")
-    for i in soup.find_all(attrs={"class": "nlc_details"}):
-        # house.append({i.text.strip("\t\n"): {}})
-        print(i.text)
+    n = int(soup.find(attrs={"class": "page"}).strong.text)/20
+    # for i in soup.find_all(attrs={"class": "nlc_details"}):
+    #     # house.append({i.text.strip("\t\n"): {}})
+    #     name = i.find(attrs={"class": "nlcd_name"}).text.strip("\t\n")
+    #     address = i.find(attrs={"class": "address"}).a["title"]
+    #     price = i.find(attrs={"class": "nhouse_price"}).text.strip("\t\n")
+    #     if name in house:
+    #         if house.get(name).get("price") == price:
+    #             pass
+    #         else:
+    #             house[name]["price"] = price
+    #     else:
+    #         house.update(
+    #             {name: {
+    #                 "price": price,
+    #                 "address": address
+    #             }})
+    # return json.dumps(house)
     # for i in soup.find_all(attrs={"class": "nhouse_price"}):
     #     if
     #     print("price: ", (i.text.strip("\t\n")))
     # for i in soup.find_all(attrs={"class": "address"}):
     #     print("address: ", (i.text.replace("\n", "").replace("\t", "")))
 
-
-
 if __name__ == "__main__":
-    print(get_data("piduqu"))
+    print(get_data("shuangliu"))
