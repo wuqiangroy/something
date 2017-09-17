9c8653f7742ca1702bdc0d4fddfed925f5320867
wuqiangroy <wuqiangroy@live.com>
2017-09-17 23:21:33 +0800

    ^_^

---
+++ fluent_python/decorator_closure/registration.py
@@ -0,0 +1,38 @@
+"""
+when python running a decorator
+"""
+
+registry = []
+
+
+def register(func):
+    """receive a function"""
+
+    print("running register {}".format(func))
+    registry.append(func)
+    return func
+
+
+@register
+def f1():
+    print("running f1()")
+
+
+@register
+def f2():
+    print("running f2()")
+
+
+def f3():
+    print("running f3()")
+
+
+def main():
+    print("running main()")
+    print("registry ->", registry)
+    f1()
+    f2()
+    f3()
+
+if __name__ == "__main__":
+    main()
