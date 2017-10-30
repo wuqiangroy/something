dbf1e45c5ecfc628bd9cf005c304478bce6d11c8
wuqiangroy <wuqiangroy@live.com>
2017-10-30 22:36:34 +0800

    pythonic class

---
+++ fluent_python/pythonic_class/vector2d_v0.py
@@ -0,0 +1,32 @@
+from array import array
+import math
+
+
+class Vector2d:
+    typecode = "d"
+
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return "{}({!r}, {!r})".format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return (bytes([ord(self.typecode)])+bytes([array(self.typecode, self)]))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
