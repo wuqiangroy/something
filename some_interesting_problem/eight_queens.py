5297e7c6a699c85a3198bed811a952040184c710
wuqiangroy <wuqiangroy@live.com>
2018-04-11 21:00:49 +0800

    prepare to fix 8 queens problem

---
+++ some_interesting_problem/eight_queens.py
@@ -0,0 +1,17 @@
+"""
+八 皇后问题
+"""
+
+
+class EightQueens:
+
+    def __init__(self):
+        pass
+
+    def solution(self):
+        return None
+
+
+if __name__ == "__main__":
+    solution = EightQueens()
+    print(solution.solution())
